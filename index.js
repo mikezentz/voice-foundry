f43efe17e314bff0f40e1f3be217af450bbf2363
Mike Zentz <mzentz@gmail.com>
2021-02-12 11:25:31 -0700

    added unit testing with mocha chai

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const wordlist = require('./wordlist.json')
 const aws = require('aws-sdk')
 const documentClient = new aws.DynamoDB.DocumentClient()
 
+exports.numberToWords = (wordlist, userPhoneNumber) => {
+  const vanityNumbers = []
+  for (const word in wordlist) {
+    if (userPhoneNumber.includes(wordlist[word].number)) {
+      const vanityNumber = userPhoneNumber.replace(wordlist[word].number, wordlist[word].word.toUpperCase())
+      const formattedVanityNumber = vanityNumber.slice(0, 3) + '-' + vanityNumber.slice(3, 6) + '-' + vanityNumber.slice(6, 10)
+      vanityNumbers.push(formattedVanityNumber)
+    }
+  }
+  return (vanityNumbers.slice(0, 5))
+}
+
 exports.handler = async (event, context, callback) => {
 
   const callernumber = event.Details.Parameters.userNumber.replace('+1', '')
 
   //AWS Connect Inbound Number 21F-EEL-S177, 213-335-7177
 
-  const numberToWords = (wordlist, userPhoneNumber) => {
-    const vanityNumbers = []
-    for (const word in wordlist) {
-      if (userPhoneNumber.includes(wordlist[word].number)) {
-        const vanityNumber = userPhoneNumber.replace(wordlist[word].number, wordlist[word].word.toUpperCase())
-        const formattedVanityNumber = vanityNumber.slice(0, 3) + '-' + vanityNumber.slice(3, 6) + '-' + vanityNumber.slice(6, 10)
-        vanityNumbers.push(formattedVanityNumber)
-      }
-    }
-    return (vanityNumbers.slice(0, 5))
-  }
-
-  const vanityNumbers = numberToWords(wordlist, callernumber)
+  const vanityNumbers = exports.numberToWords(wordlist, callernumber)
 
   const resultMap = {
     vanityNumber1: vanityNumbers[0],
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
-const test = require('./index.js')
-const expect = require('chai').expect
+const chai = require('chai')
+const expect = chai.expect
+const should = chai.should
+const wordlist = require('../wordlist.json')
 
 const event = {
   Details: {
@@ -9,4 +11,23 @@ const event = {
   }
 }
 
-console.log(test.handler(event, {}, () => {}))
+const vanity = require('../index')
+
+describe('#numberToWords', () => {
+  it('should return an array of length 5', () => {
+    let numberToWordsTest = vanity.numberToWords(wordlist, '9712492786')
+    expect(numberToWordsTest).to.have.lengthOf(5)
+  })
+})
+
+describe('#handler', () => {
+  it('should return an object with phone numbers', () => {
+    let handlerTest
+    vanity.handler(event, {}, (err, results) => {
+      handlerTest = results
+    })
+    console.log(handlerTest)
+    expect(handlerTest.vanityNumber1).to.have.lengthOf(12)
+    expect(typeof handlerTest.vanityNumber1).to.equal('string')
+  })
+})
